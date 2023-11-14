dcd51c9b7b84c66d2329904ad0f9f99910642969
I573801 <jan.wichmann@sap.com>
2023-11-14 14:32:14 +0100

    [JE]: Started working on navbar, also footer.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
+    
+    import Footer from "../_ui/layout/footer.svelte";
+    import Navbar from "../_ui/layout/navbar.svelte";
     import "../app.css";
+
+
 </script>
-  
+<Navbar />
+
 <slot />
+
+<Footer />
