916867c9d7a2482e3000f4cabe8b37ec782c14f4
I573801 <jan.wichmann@sap.com>
2023-11-15 20:51:27 +0100

    [JE]: Updated all npm packages to lts versions. Also started the animation

--- src/_ui/templates/mainRoundAbout.svelte
+++ src/_ui/templates/mainRoundAbout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    import "../../app.css";
+    import { fade } from 'svelte/transition';
+    import { onMount } from 'svelte';
+    
     import DemoUnderrated from '$lib/assets/demoUnderrated.jpeg';
-    //import DemoOppenheimer from '$lib/assets/demoOppenheimer.jpeg';
-    //import DemoSpiderVerse from '$lib/assets/demoSpiderVerse.jpeg';
-
-    let picturesToDisplay = 3;
 
     let movies: string[] = [];
 
@@ -12,13 +10,25 @@
     movies.push(DemoUnderrated);
     movies.push(DemoUnderrated);
 
-    $: displayedMovies = movies.slice(0, picturesToDisplay);
+    let activeIndex = 0;
+    let showCount = 3;
 
+    onMount(() => {
+        const interval = setInterval(() => {
+            activeIndex = (activeIndex + 1) % movies.length;
+        }, 5000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    });
 </script>
 
 <p class="text-textWhite font-bold ml-48" >FOR YOU</p>
 <div class="flex justify-center overflow-hidden">
-    {#each displayedMovies as movie}
-        <img src={movie} alt="" class="relative rounded-2xl mt-3 mb-5 ml-12 max-w-full max-h-64">
+    {#each movies as movie, i}
+        {#if i >= activeIndex && i < activeIndex + showCount}
+        <img src={movie} alt="" class="relative rounded-2xl mt-3 mb-5 ml-12 max-w-full max-h-64" in:fade={{duration: 500}}>
+        {/if}
     {/each}
 </div>
