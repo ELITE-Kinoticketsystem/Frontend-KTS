277f94a6f663590ef8443d5e4756c67a18fd184b
I573040 <elias.rw2@gmail.com>
2023-11-22 12:03:53 +0100

    [JE]: Change arrows in ImgCarousel
    Change description:
    Make arrows sharp and surrounding background smaller

--- src/_ui/templates/imageCarousel.svelte
+++ src/_ui/templates/imageCarousel.svelte
@@ -12,17 +12,17 @@
     <div class="overflow-hidden w-96 h-36rem">
         <img src={srcs[currentImg]} alt="Imajge" class="w-full h-full object-cover mx-auto" />
     </div>
-    <button class="absolute  left-0 top-1/2 -translate-y-1/2 rounded-full p-4 ml-1
-                   opacity-60 bg-white text-center hover:bg-slate-400" 
+    <button class="absolute  left-0 top-1/2 -translate-y-1/2 rounded-full p-2 ml-1
+                   opacity-60 bg-white text-center hover:bg-slate-300" 
                    on:click={decreaseIndex}>
-                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                   <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3">
                     <path d="M14 18l-6-6 6-6" />
                   </svg>
     </button>
-    <button class="absolute right-0 top-1/2 -translate-y-1/2 rounded-full p-4 mr-1
-                   opacity-60 bg-white text-center hover:bg-slate-400"
+    <button class="absolute right-0 top-1/2 -translate-y-1/2 rounded-full p-2 mr-1
+                   opacity-60 bg-white text-center hover:bg-slate-300"
                    on:click={increaseIndex}>
-                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                   <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" >
                     <path d="M10 18l6-6-6-6" />
                   </svg>
     </button>
