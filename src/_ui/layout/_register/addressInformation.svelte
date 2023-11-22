15bbaf764c60b0ce39d3a3731f1bf1376abe814b
I573801 <jan.wichmann@sap.com>
2023-11-22 09:45:38 +0100

    [JE]: Updated reigster site, however color bug
    Change Description:
    Updated complete logic. Needs to be looked at, because the progressbar, only changes color AFTER click

---
+++ src/_ui/layout/_register/addressInformation.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+    import { registrationStep } from "$lib/stores";
+
+    function nextStep() {
+        registrationStep.update((n) => n + 1);
+    }
+
+    function previousStep() {
+        registrationStep.update((n) => n - 1);
+    }
+</script>
+
+<div class="my-2 mx-2">
+    <form action="#">
+        <h3
+            class="mb-4 text-lg underline underline-offset-2 font-bold leading-none text-textWhite"
+        >
+            Address information
+        </h3>
+        <div class="grid gap-4 mb-4 sm:grid-cols-2">
+            <div>
+                <label
+                    for="coun"
+                    class="block mb-2 text-sm font-medium text-textWhite"
+                    >Country</label
+                >
+                <input
+                    type="text"
+                    name="country"
+                    id="country"
+                    class="bg-inputBlue placeholder:text-textWhite border border-gray-300 text-textWhite text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="Enter your country"
+                    autocomplete="given-name"
+                    required
+                />
+            </div>
+            <div>
+                <label
+                    for="zipcode"
+                    class="block mb-2 text-sm font-medium text-textWhite dark:text-white"
+                    >Zipcode</label
+                >
+                <input
+                    type="text"
+                    name="zipcode"
+                    id="zipcode"
+                    class="bg-inputBlue border placeholder:text-textWhite border-gray-300 text-textWhite text-sm rounded-lg block w-full p-2.5"
+                    placeholder="Enter your zipcode"
+                    required
+                />
+            </div>
+            <div>
+                <label
+                    for="city"
+                    class="block mb-2 text-sm font-medium text-textWhite"
+                    >City</label
+                >
+                <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    class="bg-inputBlue border border-gray-300 text-textWhite placeholder:text-textWhite text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5"
+                    placeholder="Enter your city"
+                    required
+                />
+            </div>
+            <div>
+                <label
+                    for="street_housenumber"
+                    class="block mb-2 text-sm font-medium text-textWhite dark:text-white"
+                    >Street + Housnumber</label
+                >
+                <input
+                    type="text"
+                    name="street_housenumber"
+                    id="street_housenumber"
+                    class="bg-inputBlue border placeholder:text-textWhite border-gray-300 text-textWhite text-sm rounded-lg block w-full p-2.5"
+                    placeholder="Enter your street"
+                    required
+                />
+            </div>
+        </div>
+        <button
+            id="previous-button"
+            type="button"
+            on:click={previousStep}
+            class="text-textWhite bg-buttonBlue duration-300 disabled:hover:bg-red-500 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+            Previous Step: General Information
+        </button>
+        <button
+            id="next-button"
+            type="button"
+            on:click={nextStep}
+            disabled
+            class="text-textWhite bg-buttonBlue duration-300 disabled:hover:bg-red-500 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+            Next Step: Review information
+        </button>
+    </form>
+</div>
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,11 +1,54 @@
 <script lang="ts">
   import { AuthService } from "$lib/_services/authService";
+  import { browser } from "$app/environment";
 
   const authService = new AuthService();
   const isUserLoggedIn = authService.isUserLoggedIn();
 
   if (isUserLoggedIn) {
     window.location.href = "/?alreadyLoggedIn=true";
+  }
+
+  $: email = "";
+  $: password = "";
+
+  $: validateButton = () => {
+    const erroMsg = document.getElementById("erroMsg")!;
+    erroMsg.innerHTML = "";
+    erroMsg.hidden = true;
+
+    const submitButton = document.getElementById("submitButton")!;
+    submitButton.disabled = !(
+      email.length > 0 &&
+      email.match(
+        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      ) &&
+      password.length > 0
+    );
+  };
+
+  async function login() {
+    const userIsLoggedIn = await authService.login(email, password);
+    if (userIsLoggedIn) {
+      if (browser) {
+        window.location.href = "/?login=true";
+      }
+    } else {
+      const errorMsg = document.getElementById("erroMsg")!;
+      const submitButton = document.getElementById("submitButton")!;
+      errorMsg.innerHTML = "Wrong e-mail or password!";
+
+      errorMsg.hidden = false;
+      submitButton.disabled = true;
+    }
+  }
+
+  function onSignIn(googleUser: any) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 </script>
 
@@ -25,6 +68,7 @@
         Login
       </h2>
     </div>
+    <!--<div class="g-signin2" data-onsuccess="onSignIn" />-->
     <button>Login with Google</button>
     <button>Login with Apple</button>
     <hr
@@ -42,6 +86,8 @@
             <input
               id="email"
               name="email"
+              bind:value={email}
+              on:input={validateButton}
               type="email"
               autocomplete="email"
               required
@@ -60,7 +106,7 @@
             >
             <div class="text-sm">
               <a
-                href="/forgot-password"
+                href="/auth/forgot-password"
                 class="font-semibold duration-300 text-buttonBlue hover:text-textWhite"
                 >Forgot password?</a
               >
@@ -71,6 +117,8 @@
               id="password"
               name="password"
               type="password"
+              bind:value={password}
+              on:input={validateButton}
               autocomplete="current-password"
               required
               class="block w-full rounded-md border-0 py-1.5 text-textWhite shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-textWhite bg-inputBlue focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -79,10 +127,19 @@
           </div>
         </div>
 
+        <p
+          hidden
+          class="flex items-center text-red-500 duration-300 font-bold leading-6 text-sm"
+          id="erroMsg"
+        />
+
         <div>
           <button
-            type="submit"
-            class="flex w-full justify-center rounded-md duration-300 bg-buttonBlue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-lg hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            disabled
+            type="button"
+            id="submitButton"
+            on:click={login}
+            class="flex w-full justify-center rounded-md duration-300 bg-buttonBlue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-lg hover:bg-green-500 disabled:hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >Login</button
           >
         </div>
@@ -91,8 +148,8 @@
       <p class="mt-10 text-center text-sm text-textWhite">
         Not a member?
         <a
-          href="/register"
-          class="font-semibold leading-6 text-indigo-500 hover:text-indigo-200"
+          href="/auth/register"
+          class="font-semibold leading-6 text-inputBlue duration-300 hover:text-textWhite"
           >Register for free</a
         >
       </p>
