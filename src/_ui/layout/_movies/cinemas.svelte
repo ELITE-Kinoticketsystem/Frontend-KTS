a062b10190d45a1d3cf6007fbfe5621a8c6cfee7
I573801 <jan.wichmann@sap.com>
2023-11-30 10:18:50 +0100

    [JE]: Removed Dropdowns

--- src/_ui/layout/_movies/cinemas.svelte
+++ src/_ui/layout/_movies/cinemas.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import DropDownMenu from "../../templates/dropDownMenu.svelte";
   import Searchbar from "../../templates/searchbar.svelte";
   import TheatreCard from "../../templates/theatreCard.svelte";
   import CinemaTileIcon from "$lib/assets/cinemaTile.png";
@@ -23,42 +22,10 @@
 
   const maxNrOfTheatresToDisplay = 12;
 
-  function filterRegion() {
-    let filteredTheatres = [];
-    for (let i = 0; i < theatres.length; ++i) {
-      if (regionsToFilter.length === 0) {
-        displayedTheatres = theatres;
-        return;
-      }
-      for (let j = 0; j < regionsToFilter.length; ++j) {
-        if (theatres[i].region === regionsToFilter[j]) {
-          filteredTheatres.push(theatres[i]);
-        }
-      }
-    }
-    displayedTheatres = filteredTheatres;
-  }
-  function filterType() {
-    let filteredTheatres = [];
-    for (let i = 0; i < theatres.length; ++i) {
-      if (typesToFilter.length === 0) {
-        displayedTheatres = theatres;
-        return;
-      }
-      for (let j = 0; j < typesToFilter.length; ++j) {
-        if (theatres[i].supportedTypes.includes(typesToFilter[j])) {
-          filteredTheatres.push(theatres[i]);
-        }
-      }
-    }
-    displayedTheatres = filteredTheatres;
-  }
   let theatre = {
     name: "Cineplex",
     logo: CinemaTileIcon,
     location: "Mannheim",
-    region: "Baden-Württemberg",
-    supportedTypes: ["2D", "3D"],
   };
 
   theatres = [
@@ -74,22 +41,16 @@
       name: "Cineplex",
       logo: CinemaTileIcon,
       location: "Stuttgart",
-      region: "Baden-Württemberg",
-      supportedTypes: ["2D"],
     },
     {
       name: "Cineplex",
       logo: CinemaTileIcon,
       location: "Mainz",
-      region: "Rheinland-Pfalz",
-      supportedTypes: ["2D", "3D"],
     },
     {
       name: "Cineplex",
       logo: CinemaTileIcon,
       location: "Bad Kreuznach",
-      region: "Rheinland-Pfalz",
-      supportedTypes: ["2D", "4D"],
     },
   ];
 
@@ -156,167 +117,6 @@
   </div>
 
   <div class="flex flex-row w-full justify-between">
-    <div class="flex">
-      <div class="flex flex-start flex-row">
-        <div class="mr-5">
-          <button
-            id="dropdownBgHoverButton"
-            data-dropdown-toggle="dropdownBgHover"
-            on:click={() => {
-              document
-                .getElementById("regionDropDown")
-                .classList.toggle("hidden");
-              document
-                .getElementById("arrowRegion")
-                .classList.toggle("rotate-180");
-            }}
-            class="text-white bg-headerBlue hover:bg-buttonBlue duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-            type="button"
-          >
-            {#key displayedTheatres}
-              Regions ({regionsToFilter.length})
-            {/key}
-            <svg
-              id="arrowRegion"
-              class="w-2.5 h-2.5 ms-3 duration-300"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          <div
-            id="regionDropDown"
-            class="z-10 hidden w-48 bg-buttonBlue rounded-lg shadow absolute origin-top mt-2"
-          >
-            {#each regions as region}
-              <ul
-                class="p-3 space-y-1 text-sm text-textWhite"
-                aria-labelledby="regionDropDown"
-              >
-                <li>
-                  <div
-                    class="flex items-center p-2 rounded hover:bg-headerBlue duration-300"
-                  >
-                    <input
-                      id="checkbox-item-{region}"
-                      type="checkbox"
-                      value=""
-                      on:click={() => {
-                        if (regionsToFilter.includes(region)) {
-                          regionsToFilter.splice(
-                            regionsToFilter.indexOf(region),
-                            1
-                          );
-                        } else {
-                          regionsToFilter.push(region);
-                        }
-                        filterRegion();
-                      }}
-                      class="w-4 h-4 text-tileBlue bg-gray-100 border-gray-300 rounded focus:ring-black focus:ring-2"
-                    />
-                    <label
-                      for="checkbox-item-{region}"
-                      class="w-full ms-2 text-sm font-medium text-textWhite rounded"
-                      >{region}</label
-                    >
-                  </div>
-                </li>
-              </ul>
-            {/each}
-          </div>
-        </div>
-        <div class="">
-          <div class="flex">
-            <div class="mr-5">
-              <button
-                id="dropdownBgHoverButton"
-                data-dropdown-toggle="dropdownBgHover"
-                on:click={() => {
-                  document
-                    .getElementById("typeDropDown")
-                    .classList.toggle("hidden");
-                  document
-                    .getElementById("arrowType")
-                    .classList.toggle("rotate-180");
-                }}
-                class="text-white bg-headerBlue hover:bg-buttonBlue duration-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-                type="button"
-              >
-                {#key displayedTheatres}
-                  Types ({typesToFilter.length})
-                {/key}
-                <svg
-                  id="arrowType"
-                  class="w-2.5 h-2.5 ms-3 duration-300"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
-              </button>
-              <div
-                id="typeDropDown"
-                class="z-10 hidden w-48 bg-buttonBlue rounded-lg shadow absolute origin-top mt-2"
-              >
-                {#each types as type}
-                  <ul
-                    class="p-3 space-y-1 text-sm text-textWhite"
-                    aria-labelledby="regionDropDown"
-                  >
-                    <li>
-                      <div
-                        class="flex items-center p-2 rounded hover:bg-headerBlue duration-300"
-                      >
-                        <input
-                          id="checkbox-item-{type}"
-                          type="checkbox"
-                          value=""
-                          on:click={() => {
-                            if (typesToFilter.includes(type)) {
-                              typesToFilter.splice(
-                                typesToFilter.indexOf(type),
-                                1
-                              );
-                            } else {
-                              typesToFilter.push(type);
-                            }
-                            filterType();
-                          }}
-                          class="w-4 h-4 text-tileBlue bg-gray-100 border-gray-300 rounded focus:ring-black focus:ring-2"
-                        />
-                        <label
-                          for="checkbox-item-{type}"
-                          class="w-full ms-2 text-sm font-medium text-textWhite rounded"
-                          >{type}</label
-                        >
-                      </div>
-                    </li>
-                  </ul>
-                {/each}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <div class="flex">
       <Searchbar
         shortSearchBarText={"Search cine..."}
