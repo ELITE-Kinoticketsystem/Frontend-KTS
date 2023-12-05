943d8659afd3ffb8479831a588995385b47bc918
I573040 <elias.rw2@gmail.com>
2023-12-05 20:42:20 +0100

    [JE]: Fix movieRow
    Change description:
    
    Ensure every movieRow entry has equal size

--- src/_ui/templates/mainCard.svelte
+++ src/_ui/templates/mainCard.svelte
@@ -7,14 +7,13 @@
   class="flex flex-col items-center w-full h-full"
   href={isActor ? "/actors/" + movie.actorId : "/movies/" + movie.movieId}
 >
-
     <img
       src={isActor ? movie.imgSrcs[1] : movie.src}
       alt={isActor ? movie.name : movie.movieName}
       class="flex h-[90%] w-max rounded-md aspect-auto object-cover mb-4"
     />
     <p
-      class="flex h-[10%] w-max text-xs sm:font-bold text-textWhite break-words text-center"
+      class="flex h-[10%] text-xs sm:text-md lg:text-lg font-bold text-textWhite break-words text-center"
     >
       {isActor ? movie.name : movie.movieName}
     </p>
