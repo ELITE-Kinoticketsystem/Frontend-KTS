13d31a918e44fcc708781039ca6d8c30cd99b520
I573040 <elias.rw2@gmail.com>
2023-12-12 12:42:43 +0100

    [JE]: AdminSeatSelection
    Change description:

---
+++ src/_ui/templates/plusButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<button class="w-full h-full round-full bg-green-400">
+  <svg preserveAspectRatio="xMidYMid meet" width="100%" height="100%" viewBox="0 0 100 100">
+    <rect width="80" height="20" x="10" y="40" fill="#ffffff"/>
+    <rect width="20" height="80" x="40" y="10" fill="#ffffff"/>
+  </svg>
+</button>
--- src/routes/admin/mycinema/add/+page.svelte
+++ src/routes/admin/mycinema/add/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    
+  import HallCreator from "../../../../_ui/templates/hallCreator.svelte";
+
 </script>
 
 
-<div class="">
-    
+<div class="w-[70%] h-[80vh] mx-auto">
+    <HallCreator/>
 </div>
