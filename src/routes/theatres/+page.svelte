4e7c9927ed717f340c4a2dd68183aadecb8660c5
I573801 <jan.wichmann@sap.com>
2024-01-10 10:13:19 +0100

    [JE]: New Theatre Page

--- src/routes/theatres/+page.svelte
+++ src/routes/theatres/+page.svelte
@@ -31,66 +31,77 @@
 </script>
 
 <head:svelte>
-  <title>Cinemika - Locations</title>
+  <title>Cinemika - Theatresg</title>
 </head:svelte>
 
-<div class="sm:w-0 md:w-[5%] lg:w-1/6 xl:1/4 2xl:1/3 flex-shrink-0" />
-<div class="flex flex-col flex-grow w-max mx-auto">
-  <div class="mx-auto text-textWhite my-5">
-    <span class="text-2xl">Current selection: </span>
-    <span class="font-bold text-2xl"> {cinema}</span>
-    <button
-      class="text-textWhite text-lg py-px px-2 hover:bg-tileBlue duration-300 bg-buttonBlue rounded-lg"
-      on:click={() => {
-        if (browser) {
-          localStorage.setItem("cinema", "All");
-          goto("/");
-        }
-      }}>Skip</button
+<div class="flex w-screen h-max">
+  <div class="sm:w-0 md:w-0 lg:w-1/6 xl:1/4 2xl:1/3 flex-shrink-0" />
+  <div class="flex flex-col w-full h-full flex-grow mx-auto">
+    <div
+      class="flex flex-row mx-auto text-textWhite my-2 w-full justify-between"
     >
-  </div>
-  <div class="mx-5">
-    <Searchbar
-      shortSearchBarSize="w-full"
-      longSearchBarSize="w-full"
-      shortSearchBarText="Search for a location"
-      longSearchBarText="Search for a location"
-      on:inputChange={(event) => {
-        let key = event.detail.toLowerCase().trim();
-        filterLocations(key);
-      }}
-    />
-  </div>
-  <div class="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4 mt-5">
-    {#each displayedLocations as location}
-      <div
-        class="flex flex-col card bg-tileBlue text-white rounded-lg shadow-lg overflow-hidden"
-      >
-        <img
-          class="h-48 w-full object-cover"
-          src={location.LogoURL}
-          alt={location.Name}
+      <div class="justify-start w-1/2">
+        <Searchbar
+          shortSearchBarSize="w-[38%]"
+          longSearchBarSize="w-2/3"
+          shortSearchBarText="Search for a theatre"
+          longSearchBarText="Search for a theatre"
+          on:inputChange={(event) => {
+            let key = event.detail.toLowerCase().trim();
+            filterLocations(key);
+          }}
         />
-        <div class="flex flex-col p-6">
-          <h2 class="text-lg font-semibold">
-            {location.Name}
-          </h2>
-          <p class="text-sm">Location WIP</p>
+      </div>
+      <div class="justify-end flex flex-row">
+        <span class="text-2xl"
+          >{cinema === "All" ? "Theatres: " : "Theatre:"}</span
+        >
+        <span class="font-bold text-2xl ml-1"> {cinema}</span>
+        <div class="ml-2 mt-px">
           <button
-            class="mt-4 bg-buttonBlue hover:bg-green-800 duration-300 text-white font-bold py-2 px-4 rounded"
+            class="text-textWhite text-lg py-px px-1 hover:bg-tileBlue duration-300 bg-buttonBlue rounded-lg"
             on:click={() => {
               if (browser) {
-                localStorage.setItem("cinema", location.Name);
-                localStorage.setItem("cinemaId", location.ID);
+                localStorage.setItem("cinema", "All");
                 goto("/");
               }
-            }}
+            }}>Skip</button
           >
-            Select
-          </button>
         </div>
       </div>
-    {/each}
+    </div>
+    <hr />
+    <div class="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-4 mt-5">
+      {#each displayedLocations as location}
+        <div
+          class="flex flex-col card bg-tileBlue text-white rounded-lg shadow-lg overflow-hidden"
+        >
+          <img
+            class="h-48 w-full object-cover"
+            src={location.LogoURL}
+            alt={location.Name}
+          />
+          <div class="flex flex-col p-6">
+            <h2 class="text-lg font-semibold">
+              {location.Name}
+            </h2>
+            <p class="text-sm">Location WIP</p>
+            <button
+              class="mt-4 bg-buttonBlue hover:bg-green-500 duration-300 text-white font-bold py-2 px-4 rounded"
+              on:click={() => {
+                if (browser) {
+                  localStorage.setItem("cinema", location.Name);
+                  localStorage.setItem("cinemaId", location.ID);
+                  goto("/");
+                }
+              }}
+            >
+              Select
+            </button>
+          </div>
+        </div>
+      {/each}
+    </div>
   </div>
+  <div class="sm:w-0 md:w-0 lg:w-1/6 xl:1/4 2xl:1/3 flex-shrink-0" />
 </div>
-<div class="sm:w-0 md:w-[5%] lg:w-1/6 xl:1/4 2xl:1/3 flex-shrink-0" />
